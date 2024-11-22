ec148c0504f3b16607d70a9613ad0940bd522227
Bahia <byudachkin>
2024-11-22 12:56:22 -0300

    emepce con js de categories

--- js/categories.js
+++ js/categories.js
@@ -1,3 +1,31 @@
 fetch('https://dummyjson.com/recipes/tags')
 .then(res => res.json())
 .then(console.log);
+
+const categorias = document.getElementById("categorias");
+
+function buscarCategorias(){
+    fetch(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        if (data.recipes && data.recipes.lenght > 0){
+            let categoria= [];
+            for(let i = 0; i < data.recipes.lenght; i++){
+                const comida = data.recipes[i];
+                if(comida.tags){
+                    for (let h = 0; h < comida.tegs.lenght; h++){
+                        const tag = comida.tags[h];
+                        let existente = false;
+                        for (let g = 0; g < categoria.length; g++){
+                            if(categoria[g] === tag){
+                                existente= true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+}
