37e3642682d47dfd3eb8d1ffbd8ab26624a750dc
Bahia <byudachkin>
2024-11-27 23:02:00 -0300

    realizado categories js

--- js/categories.js
+++ js/categories.js
@@ -1,31 +1,16 @@
 fetch('https://dummyjson.com/recipes/tags')
-.then(res => res.json())
-.then(console.log);
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    let categorias = document.querySelector(".categorias");
 
-const categorias = document.getElementById("categorias");
-
-function buscarCategorias(){
-    fetch(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        if (data.recipes && data.recipes.lenght > 0){
-            let categoria= [];
-            for(let i = 0; i < data.recipes.lenght; i++){
-                const comida = data.recipes[i];
-                if(comida.tags){
-                    for (let h = 0; h < comida.tegs.lenght; h++){
-                        const tag = comida.tags[h];
-                        let existente = false;
-                        for (let g = 0; g < categoria.length; g++){
-                            if(categoria[g] === tag){
-                                existente= true;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    })
-
-}
+    lista= "";
+    for (let i = 0; i < data.lenght; i++) {
+        lista += `<li><a href="category.html?tag=${data[i]}">${data[i]}</a></li>`
+    }
+    categorias.innerHTML = lista;
+})
+.catch(function(error){
+    console.log(error);
+})
